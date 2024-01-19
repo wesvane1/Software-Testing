fa9755ca28b98847f2f736cd044b86c4a39dac0f
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-01-19 15:20:55 -0700

    Got everything.. except getting the DB to connect.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-const routes = require('express').Router();
-// There are two dots in the file path because we need to leave the routes folder so that we can make it to the controllers folder
-const lesson1Controller = require('../controllers/lesson1')
- 
-routes.get('/', lesson1Controller.savannaRoute);
-routes.get('/doggie', lesson1Controller.peachesRoute);
-routes.get('/brother', lesson1Controller.brotherRoute)
+const express = require('express');
+const router = express.Router();
 
-module.exports = routes;
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
 const app = express();
 
-const port = 3000;
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.use('/', require('./routes'));
- 
-app.listen(process.env.PORT || port);
-console.log('Web Server is listening at port ' + (process.env.PORT || port));
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
