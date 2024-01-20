9c849d7a7f1f8f3e61d9ad6b8df2feefb5287b58
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-01-20 10:37:50 -0700

    still trying lol

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db().collection('testmon').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
