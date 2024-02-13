ca7b82c46478f2510ac35351633f12b60e70a267
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-02-13 12:24:16 -0700

    Part 2.2 fixing validation

--- validation.js
+++ validation.js
@@ -4,7 +4,12 @@ exports.birthdayValidation = [
     // check('name', 'Name is requied').not().isEmpty(),
     // check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
     // check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
-    check('birthday', 'please input birthday as MM/DD/YYYY').isLength({min: 10})
+    check('birthday', 'please input birthday as MM/DD/YYYY').custom((value, { req }) => {
+        if (!moment(value, 'MM/DD/YYYY', true).isValid()) {
+            throw new Error('Please enter a valid date in MM/DD/YYYY format');
+        }
+        return true;
+    })
 ]
 
 // exports.loginValidation = [
